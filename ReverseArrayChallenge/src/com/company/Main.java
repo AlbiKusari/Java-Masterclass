10ac2ee4807235da4f83b27426d33673a6543c60
Albi Kusari <albi_kusa@hotmail.com>
2021-08-26 01:23:13 +0200

    ReverseArrayChallenge

---
+++ ReverseArrayChallenge/src/com/company/Main.java
@@ -0,0 +1,40 @@
+package com.company;
+
+import java.lang.reflect.Array;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int [] array1 = {1 , 2 , 3 , 4 , 5};
+        reverse(array1);
+        System.out.println();
+        int [] array2 = {1 , 3 , 5 , 6 , 7 , 8};
+        reverse(array2);
+
+    }
+
+    public static void reverse(int [] array) {
+
+        System.out.print("Array: ");
+        for (int i : array)
+            System.out.print(i + " ");
+        System.out.println();
+
+        int temp;
+        int count = 0;
+
+        for (int i = array.length-1; i >= array.length/2 ; i--){
+            temp = array[count];
+            array[count] = array[i];
+            array[i] = temp;
+            count++;
+        }
+        System.out.print("Reversed array: ");
+        for (int i : array)
+            System.out.print(i + " ");
+        System.out.println();
+
+            }
+        }
+
